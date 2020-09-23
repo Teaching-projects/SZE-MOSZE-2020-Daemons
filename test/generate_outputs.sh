7a3ba5746ba44fefc8b16fffb7d92e1a9052f26c
matyko <mesicsmatyas@gmail.com>
2020-09-23 22:55:16 +0200

    Test for pass.

--- test/generate_outputs.sh
+++ test/generate_outputs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+mkdir outputs
 declare -a files
 
 for filename in test/units/*.json; do
@@ -12,10 +12,8 @@ for ((c=0;c<$((fileCount-1));c++))
 do
 	for ((i=$c;i<$((fileCount-1));i++))
 	do
-		rm test/outputs/${files[$((i+1))]}-${files[$c]}.txt
 		touch test/outputs/${files[$((i+1))]}-${files[$c]}.txt
 		echo `./a.out test/units/${files[$((i+1))]}.json test/units/${files[$c]}.json` >> test/outputs/${files[$((i+1))]}-${files[$c]}.txt
-		rm test/outputs/${files[$c]}-${files[$((i+1))]}.txt
 		touch test/outputs/${files[$c]}-${files[$((i+1))]}.txt
 		echo `./a.out test/units/${files[$c]}.json test/units/${files[$((i+1))]}.json` >> test/outputs/${files[$c]}-${files[$((i+1))]}.txt
 	done
