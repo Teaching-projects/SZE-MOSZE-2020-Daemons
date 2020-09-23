a879aa99673eef907276a3dec60bb6d3be9f5a38
matyko <mesicsmatyas@gmail.com>
2020-09-23 21:01:02 +0200

    Correct generator srcipt.

--- test/generate_outputs.sh
+++ test/generate_outputs.sh
@@ -5,6 +5,7 @@ declare -a files
 for filename in units/*.json; do
 	files+=(`echo $filename | grep -o -P '(?<=units/).*(?=.json)'`)
 done
+cd ..
 
 fileCount=${#files[@]}
 
@@ -12,11 +13,11 @@ for ((c=0;c<$((fileCount-1));c++))
 do
 	for ((i=$c;i<$((fileCount-1));i++))
 	do
-		rm outputs/${files[$((i+1))]}-${files[$c]}.txt
-		touch outputs/${files[$((i+1))]}-${files[$c]}.txt
-		echo `~/SZE-MOSZE-2020-Daemons/a.out units/${files[$((i+1))]}.json units/${files[$c]}.json` >> ~/SZE-MOSZE-2020-Daemons/test/outputs/${files[$((i+1))]}-${files[$c]}.txt
-		rm outputs/${files[$c]}-${files[$((i+1))]}.txt
-		touch outputs/${files[$c]}-${files[$((i+1))]}.txt
-		echo `~/SZE-MOSZE-2020-Daemons/a.out units/${files[$c]}.json units/${files[$((i+1))]}.json` >> ~/SZE-MOSZE-2020-Daemons/test/outputs/${files[$c]}-${files[$((i+1))]}.txt
+		rm test/outputs/${files[$((i+1))]}-${files[$c]}.txt
+		touch test/outputs/${files[$((i+1))]}-${files[$c]}.txt
+		echo `./a.out test/units/${files[$((i+1))]}.json test/units/${files[$c]}.json` >> test/outputs/${files[$((i+1))]}-${files[$c]}.txt
+		rm test/outputs/${files[$c]}-${files[$((i+1))]}.txt
+		touch test/outputs/${files[$c]}-${files[$((i+1))]}.txt
+		echo `./a.out test/units/${files[$c]}.json test/units/${files[$((i+1))]}.json` >> test/outputs/${files[$c]}-${files[$((i+1))]}.txt
 	done
 done
