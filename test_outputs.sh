efb3172b97bc411296cd9065a7c43692e5118717
matyko <mesicsmatyas@gmail.com>
2020-09-23 19:24:50 +0200

    Remove comments.

--- test_outputs.sh
+++ test_outputs.sh
@@ -4,7 +4,6 @@ differences=0
 
 for outputfile in outputs/*.txt
 do
-	#echo $"good_outputs${outputfile//outputs}"
 	otherdiffs=$(diff $"good_outputs${outputfile//outputs}" $outputfile)
 	differences=$((differences+otherdiffs))
 done
