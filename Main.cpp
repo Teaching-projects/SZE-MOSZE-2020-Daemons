29573a0a7dad98b9377ef18d1a529d1071e8802f
Lazar-source <56961536+Lazar-source@users.noreply.github.com>
2020-09-24 10:59:42 +0200

    Fixing players delete

--- Main.cpp
+++ Main.cpp
@@ -4,14 +4,35 @@
 
 int main(int argc,char** argv)
 {
+	Unit* player1;
+	Unit* player2;
 	std::vector<std::string> args(argc);
 	for(int i=0; i<argc; ++i){
 		args[i] = argv[i];
 	}
+
 	try{
-		Unit* player1 = Unit::parseUnit(args[1]);
-		Unit* player2 = Unit::parseUnit(args[2]);
-		Control::Attack(*player1,*player2);
+		 player1 = Unit::parseUnit(args[1]);
+		//Unit* player2 = Unit::parseUnit(args[2]);
+		
+	}catch(std::exception &e){
+		std::cout << "Oops, " << e.what() << std::endl;
+		return 1;
+	}
+
+	try{
+		 player2 = Unit::parseUnit(args[2]);
+
+	}
+	catch(std::exception &e)
+	{
+		std::cout << "Oops, " << e.what() << std::endl;
+		delete player1;
+		return 1;
+		
+	}
+
+	Control::Attack(*player1,*player2);
 		if (player1->getHp() == 0)
 		{
 			std::cout << player2->getName() << " wins. Remaining HP: " << player2->getHp() << std::endl;
@@ -21,10 +42,6 @@ int main(int argc,char** argv)
 			std::cout << player1->getName() << " wins. Remaining HP: " << player1->getHp() << std::endl;
 		}
 		delete player1, player2;
-	}catch(std::exception &e){
-		std::cout << "Oops, " << e.what() << std::endl;
-		return 1;
-	}
 
 	return 0;
 }
