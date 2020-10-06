35d87a64a080a1f50615177062e26763fc5e5e3f
kulcsarbence <bance98@gmail.com>
2020-10-06 13:11:44 +0200

    Minor change to test

--- test/test_outputs.sh
+++ test/test_outputs.sh
@@ -4,8 +4,9 @@ differences=0
 cd test
 for outputfile in good_outputs/*.txt
 do
-	otherdiffs=`diff $"${outputfile//good_}" $outputfile`
-	differences+=$otherdiffs
+	echo "`diff $"${outputfile//good_}" $outputfile`"
+	diff $"${outputfile//good_}" $outputfile > /dev/null 2>&1
+	differences+=$?
 done
 
 exit $differences
