6ebd18ce0fc42ff42fbdc928d09bb2bbc22e1937
matyko <mesicsmatyas@gmail.com>
2020-10-12 09:44:45 +0200

    Build for pass

--- buildcheck.sh
+++ buildcheck.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-g++ -Wall -std=c++17 *.cpp &>> output.txt
-cppcheck *.cpp --enable=warning &>> output.txt
+g++ -Wall -std=c++17 *.cpp &> output.txt
 if [ -s output.txt ]
 then
         cat output.txt
