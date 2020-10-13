539ff122591d53bb8066464d4b35a42394f5f81b
kulcsarbence <32749705+kulcsarbence@users.noreply.github.com>
2020-10-13 09:41:29 +0200

    Minor change

--- JsonParser.cpp
+++ JsonParser.cpp
@@ -25,7 +25,9 @@ std::map<std::string, std::string> JsonParser::parseJSON(const std::string& data
       toParse = data;
     }
     toParse.erase(remove_if(toParse.begin(), toParse.begin() + JsonParser::nthOccurrence(toParse, "\"", 1), isspace), toParse.begin() + JsonParser::nthOccurrence(toParse, "\"", 1));
+    std::cout << "debug> " << toParse << std::endl;
     toParse.erase(remove_if(toParse.begin() + JsonParser::nthOccurrence(toParse, "\"", 2), toParse.end(), isspace), toParse.end());
+    std::cout << "debug 2> " << toParse << std::endl;
     std::string charactersToRemove = "{}";
     for(auto& ch : charactersToRemove){
       toParse.erase(std::remove(toParse.begin(), toParse.end(), ch));
