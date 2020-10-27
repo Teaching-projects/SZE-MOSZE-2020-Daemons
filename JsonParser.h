892375141f5e2058b7e926380f3c666b18f26196
Lazar-source <56961536+Lazar-source@users.noreply.github.com>
2020-10-27 12:28:10 +0100

    Correct requested Changes

--- JsonParser.h
+++ JsonParser.h
@@ -32,6 +32,7 @@ private:
 	 \param str String parameter
    \param findMe const string that the method will find in the str parameter
    \param nth int parameter that represents how many findMe string we will find (default 1)
+   \return the method returns the position of the founded character 
 	*/
   static int nthOccurrence(const std::string& str, const std::string& findMe, int nth);
 public:
@@ -43,7 +44,7 @@ public:
 	*/
   static std::map<std::string, std::string> parseJSON(std::istream& data);
   //! Method that parsed the the values from the lines which he get in parameter 
-   //!it will throw runtime error if there is something it cant find. For Example: name-> Name key not found
+   //! it will throw runtime error if there is something it cant find. For Example: name-> Name key not found
     /*!
 	 \param data Parsed lines in a Map
    \return Parsed Values in a Map
