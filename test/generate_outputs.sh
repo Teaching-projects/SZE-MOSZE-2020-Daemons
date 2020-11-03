aea60dc2653b8361bb22d73736c0edcc187d6ac5
matyko <mesicsmatyas@gmail.com>
2020-11-03 13:46:17 +0100

    Update in/out tests

--- test/generate_outputs.sh
+++ test/generate_outputs.sh
@@ -2,9 +2,6 @@
 touch test/outputs.txt
 declare -a files
 
-for filename in test/units/*.json; do
-	files+=(`echo $filename | grep -o -P '(?<=units/).*(?=.json)'`)
-done
 
 fileCount=${#files[@]}
 
@@ -12,7 +9,7 @@ for ((c=0;c<$((fileCount-1));c++))
 do
 	for ((i=$c;i<$((fileCount-1));i++))
 	do
-		echo `./runMain test/units/${files[$((i+1))]}.json test/units/${files[$c]}.json` >> test/outputs.txt
-		echo `./runMain test/units/${files[$c]}.json test/units/${files[$((i+1))]}.json` >> test/outputs.txt
+		echo `./runMain scenario1.json` >> test/outputs.txt
+		echo `./runMain scenario1.json` >> test/outputs.txt
 	done
 done
