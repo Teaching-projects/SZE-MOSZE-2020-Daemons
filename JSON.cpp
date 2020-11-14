352bc270767d34c1cab4008108f584aee8e90d72
kulcsarbence <bance98@gmail.com>
2020-11-14 18:22:18 +0100

    Switched to regex, because the previous version was not sustainable

--- JSON.cpp
+++ JSON.cpp
@@ -7,7 +7,15 @@ JSON JSON::parseFromFile(const std::string &filename)
         throw ParseException("Error opening file");
     }
 
-    return parseStream(file);
+    std::smatch allMatches;
+    static const std::regex fnameCheck("([\\w]*).json$");
+    if(std::regex_search(filename, allMatches, fnameCheck)){
+      std::map <std::string, std::variant<std::string, int, double>> toReturn = parseStream(file).data_map;
+      file.close();
+      return JSON(toReturn);
+    }else{
+      return parseJSON(filename);
+    }
 }
 
 JSON JSON::parseStream(std::istream& data){
@@ -17,92 +25,51 @@ JSON JSON::parseStream(std::istream& data){
     toParse += line;
   }
 
-  return JSON::parseJSON(toParse);
+  return parseJSON(toParse);
 }
 
 JSON JSON::parseJSON(const std::string& data){
-  try{
-    std::map<std::string,std::variant<std::string, int, double>>  toReturn;
-    std::ifstream test(data);
-    std::string toParse = "";
-
-    if(test){
-      std::string line;
-      while(std::getline(test, line)){
-        toParse += line + "\n";
-      }
-    }else{
-      toParse = data;
-    }
-    std::string new_str;
-    bool delete_spaces = true, start = false;
-    for(unsigned int i = 0; i < toParse.size(); ++i) {
-        if(toParse[i] == '\"') {
-            start ? start = false : start = true;
-            if(start) {
-                delete_spaces = false;
-            }
-        }
-        if(!start) {
-            delete_spaces = true;
-        }
-        if(delete_spaces) {
-            if(toParse[i] != ' ' && toParse[i] != '\n' && toParse[i] != '\t') {
-                new_str += toParse[i];
-            }
-        } else {
-            new_str += toParse[i];
-        }
-
-    }
-    std::string charactersToRemove = "{}";
-    for(auto& ch : charactersToRemove){
-      new_str.erase(std::remove(new_str.begin(), new_str.end(), ch));
-    }
-
-    std::string toProcess = new_str;
-
-    std::string key;
-    std::string value;
-    while(JSON::nthOccurrence(toProcess, ",", 1) != -1){
-      key = toProcess.substr(0, JSON::nthOccurrence(toProcess, ":", 1));
-      key.erase(std::remove(key.begin(), key.end(), '\"'), key.end());
-      value = toProcess.substr(JSON::nthOccurrence(toProcess, ":", 1)+1, JSON::nthOccurrence(toProcess, ",", 1)-JSON::nthOccurrence(toProcess, ":", 1)-1);
-      value.erase(std::remove(value.begin(), value.end(), '\"'), value.end());
-      if (!value.empty() && std::all_of(value.begin(), value.end(), [](char c){return std::isdigit(c);})) toReturn[key] = std::stoi(value);
-      else if (!value.empty() && std::all_of(value.begin(), value.end(), [](char c){return ((std::isdigit(c) || c == '.') ? true : false);})) toReturn[key] = std::stod(value);
-      else toReturn[key] = value;
-      toProcess = toProcess.substr(JSON::nthOccurrence(toProcess, ",", 1)+1);
-      if(JSON::nthOccurrence(toProcess, ",", 1) == -1){
-        key = toProcess.substr(0, JSON::nthOccurrence(toProcess, ":", 1));
-        key.erase(std::remove(key.begin(), key.end(), '\"'), key.end());
-        value = toProcess.substr(JSON::nthOccurrence(toProcess, ":", 1)+1, JSON::nthOccurrence(toProcess, ",", 1)-JSON::nthOccurrence(toProcess, ":", 1)-1);
-        value.erase(std::remove(value.begin(), value.end(), '\"'), value.end());
-        if (!value.empty() && std::all_of(value.begin(), value.end(), [](char c){return std::isdigit(c);})) toReturn[key] = std::stoi(value);
-        else if (!value.empty() && std::all_of(value.begin(), value.end(), [](char c){return ((std::isdigit(c) || c == '.') ? true : false);})) toReturn[key] = std::stod(value);
-        else toReturn[key] = value;
-      }
-    }
-    return JSON(toReturn);
-  }catch(std::exception& e){
-    throw ParseException(e.what());
+  static const std::regex regexForParse("\\s*\"([\\w]*)\"\\s*:\\s*\"?([\\s\\w\\.]*)\"?\\s*[,}]\\s*");
+  static const std::regex regexForList("\\s*\"([\\w]*)\"\\s*:\\s*\"?\\[?\\s*([\\w\\.\"?,?\\s*]*)\"?\\s*[,\\]}]");
+  std::smatch allMatches;
+  std::smatch allMatchesList;
+  std::map <std::string, std::variant<std::string, int, double>> map_store;
+  if (data.substr(0,1) != "{"){
+      throw ParseException("Missing { at the start.");
   }
-}
+  else if (data.substr(data.size()-1, 1) != "}"){
+      throw ParseException("Missing { at the end.");
+  }
 
-int JSON::nthOccurrence(const std::string& str, const std::string& findMe, int nth)
-{
-    size_t  pos = 0;
-    int     cnt = 0;
+  while(std::regex_search(data, allMatches, regexForParse)){
+      if (allMatches[1] == "") {
+          throw ParseException("Incorrect key");
+      }
+      else if (allMatches[2] == "") {
+          throw ParseException("Incorrect value");
+      }
 
-    while( cnt != nth )
-    {
-        pos+=1;
-        pos = str.find(findMe, pos);
-        if ( pos == std::string::npos )
-            return -1;
-        cnt++;
-    }
-    return pos;
+      else
+      {
+          std::string value = allMatches[2];
+          if (!value.empty() && std::all_of(value.begin(), value.end(), [](char c){return std::isdigit(c);})) map_store[allMatches[1]] = std::stoi(value);
+          else if (!value.empty() && std::all_of(value.begin(), value.end(), [](char c){return ((std::isdigit(c) || c == '.') ? true : false);})) map_store[allMatches[1]] = std::stod(value);
+          else map_store[allMatches[1]] = value;
+          data = allMatches.suffix().str();
+      }
+  }
+  if(std::regex_search(data, allMatchesList, regexForList)){
+      std::string values = allMatchesList[2];
+      while (values.find(",")!=std::string::npos)
+          values.erase(values.find(","),1);
+
+      while(values.find("\"")!= std::string::npos)
+          values.erase(values.find("\""),1);
+
+      map_store[allMatchesList[1]]=values;
+      data = allMatchesList.suffix().str();
+  }
+  return JSON(map_store);
 }
 
 unsigned int JSON::count(const std::string& input){
--- JSON.h
+++ JSON.h
@@ -1,17 +1,17 @@
 
 /**
  * \class JSON
- * 
+ *
  * \brief  JSON class
- * 
+ *
  * This class Parses the Json Files into data, that stored in  Unit Class
- * 
+ *
  * \author  mesmatyi, kulcsarbence, Lazar-Source
- * 
+ *
  * \version 1.0
- * 
+ *
  * \date 2020/10/026 13:09
- * 
+ *
  * Created on 2020/10/26 13:09
  */
 
@@ -24,6 +24,10 @@
 #include <variant>
 #include <fstream>
 #include <algorithm>
+#include <regex>
+#include <cctype>
+#include <type_traits>
+
 
 class JSON{
 private:
@@ -32,33 +36,44 @@ private:
 	 \param str String parameter
    \param findMe const string that the method will find in the str parameter
    \param nth int parameter that represents how many findMe string we will find (default 1)
-   \return the method returns the position of the founded character 
+   \return the method returns the position of the founded character
 	*/
   static int nthOccurrence(const std::string& str, const std::string& findMe, int nth);
-  //! Map that stores the Json file 
+  //! Map that stores the Json file
   std::map <std::string, std::variant<std::string, int, double>> data_map;
 public:
   JSON(std::map <std::string, std::variant<std::string, int, double>> data) : data_map(data){}; //!constructor
+  typedef std::list<std::variant<std::string, int, double>> list;
 
   unsigned int count(const std::string& input);
 
-  std::variant<std::string, int, double> operator [] (std::string key) {
-    return data_map.find(key)->second;
-  }
   //! Template to get the data from the map in different types.
   template <typename T>
-  T get(const std::string& input){
-    T toReturn = std::get<T>(data_map[input]);
-    return toReturn;
+	inline typename std::enable_if<std::is_same<T, JSON::list>::value, T>::type
+  get(const std::string& key)
+  {
+	if (!count(key)){
+    throw ParseException("The given key doesnt exist in our map");
+  } else{
+      list toReturn;
+      std::istringstream values(std::get<std::string>(data_map[key]));
+      std::copy(std::istream_iterator<std::string>(values), std::istream_iterator<std::string>(), std::back_inserter(toReturn));
+      return toReturn;
   }
+}
+
+template <typename T> inline typename std::enable_if<!std::is_same<T, JSON::list>::value, T>::type
+	get(const std::string& key){
+      if (!count(key)) throw ParseException("The given key doesnt exist in our map");
+      else return std::get<T>(data_map[key]);
 //! Method that read in the lines from the Json file
     /*!
 
-     \return Parsed lines in a Map 
-	   \param data  Json file 
+     \return Parsed lines in a Map
+	   \param data  Json file
 	*/
   static JSON parseStream(std::istream& data);
-  //! Method that parsed the the values from the lines which he get in parameter 
+  //! Method that parsed the the values from the lines which he get in parameter
    //! it will throw runtime error if there is something it cant find. For Example: name-> Name key not found
     /*!
 	 \param data Parsed lines in a Map
@@ -67,6 +82,11 @@ public:
   static JSON parseJSON(const std::string& data);
   //! Method that represents if the File can be open, or not.
    static JSON parseFromFile(const std::string& data);
+
+   template <typename T> inline typename std::enable_if<!std::is_same<T, JSON::list>::value, T>::type
+   get(const std::string& key){
+         if (!count(key)) throw ParseException("The given key doesnt exist in our map");
+         else return std::get<T>(data_map[key]);
 
   class ParseException : public std::runtime_error{
     public:
