6553fa65a1761b591cb1ce155799b3a9aecbbd05
Lazar-source <56961536+Lazar-source@users.noreply.github.com>
2020-11-19 08:26:07 +0100

    Added map.h and map.cpp

---
+++ Map.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <map>
+#include <string>
+#include <vector>
+
+class Map{
+
+private:
+ std::vector<std::string> vectormap;
+ 
+ public:
+        enum type {Free, Wall};
+        Map(std::string filename);
+        Map::type get(unsigned int x, unsigned int y) const;
+
+    
+    class WrongIndexException : virtual public std::runtime_error {
+        public:
+            
+         WrongIndexException(const std::string &err) : std::runtime_error( err) {}
+    };
+};
