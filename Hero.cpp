99921b482b165e8b3597a756feeb08faf32ffc7f
Lazar-source <56961536+Lazar-source@users.noreply.github.com>
2020-11-23 20:20:21 +0100

    let see if it works

--- Hero.cpp
+++ Hero.cpp
@@ -20,7 +20,6 @@ Hero Hero::parse(const std::string& data){
 	// std::cout << returnedMap.get<int>("defense_bonus_per_level")<<std::endl;
 	// /*std::cout << typeid(returnedMap.get<int>("defense")).name()<<std::endl;
 	// std::cout << typeid(returnedMap.get<int>("defense_bonus_per_level")).name()<<std::endl;
-	// std::cout << typeid(returnedMap.get<int>("magical_damage_bonus_per_level")).name()<<std::endl;*/
 
 	return Hero(
 	returnedMap.get<int>("base_health_points"),
