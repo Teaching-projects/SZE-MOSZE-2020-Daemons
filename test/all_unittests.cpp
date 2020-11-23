10c96883e8860104ff1a2fbb841e59cf55c9826c
Lazar-source <56961536+Lazar-source@users.noreply.github.com>
2020-11-23 20:51:24 +0100

    fix unittest

--- test/all_unittests.cpp
+++ test/all_unittests.cpp
@@ -1,6 +1,7 @@
 #include "../JSON.h"
 #include "../Hero.h"
 #include "../Monster.h"
+#include "../Damage.h"
 #include <gtest/gtest.h>
 #include <stdio.h>
 #include <iostream>
@@ -12,7 +13,7 @@ TEST(all_unitsTest,hero_load_test)
     Hero hero{Hero::parse("../Dark_Wanderer.json")};
     ASSERT_TRUE(hero.getHealthPoints() == 30);
     ASSERT_TRUE(hero.getName() == "Prince Aidan of Khanduras");
-    ASSERT_TRUE(hero.getDamage() == 3);
+    ASSERT_TRUE(hero.getphysDamage() == 3);
     ASSERT_TRUE(hero.getAttackCoolDown() == 1.1);
 }
 TEST(all_unitsTest,Monster_load_test)
@@ -20,7 +21,7 @@ TEST(all_unitsTest,Monster_load_test)
     Monster monster{Monster::parse("../Zombie.json")};
     ASSERT_TRUE(monster.getHealthPoints() == 10);
     ASSERT_TRUE(monster.getName() == "Zombie");
-    ASSERT_TRUE(monster.getDamage() == 1);
+    ASSERT_TRUE(monster.getphysDamage() == 1);
     ASSERT_TRUE(monster.getAttackCoolDown() == 2.8);
 }
 TEST(all_unitsTest,Unit_edge_case_file)
@@ -37,7 +38,7 @@ TEST(all_unitsTest,typetest)
     EXPECT_EQ(typeid(int),typeid(hero.getHealthPoints()));
     EXPECT_EQ(typeid(std::string),typeid(hero.getName()));
     EXPECT_EQ(typeid(double),typeid(hero.getAttackCoolDown()));
-    EXPECT_EQ(typeid(int),typeid(hero.getDamage()));
+    EXPECT_EQ(typeid(int),typeid(hero.getphysDamage()));
     
 }
 TEST(all_unitsTest,Unit_fight)
@@ -46,21 +47,6 @@ TEST(all_unitsTest,Unit_fight)
     Monster monster{Monster::parse("../Zombie.json")};
     hero.fightTilDeath(monster);
     ASSERT_TRUE(monster.getHealthPoints() < hero.getHealthPoints());
-}
-TEST(all_unitsTest,Generate_hero)
-{
-    Hero hero(30,10,"Hero_of_the_heroes",1.202,2,5,1,0.011);
-    Monster monster(30,10,"Enemy",0.02);
-    ASSERT_EQ(hero.getName(),"Hero_of_the_heroes");
-    ASSERT_EQ(hero.getHealthPoints(),30);
-    ASSERT_EQ(hero.getDamage(),10);
-    ASSERT_EQ(hero.getAttackCoolDown(),1.202);
-
-    ASSERT_EQ(monster.getName(),"Enemy");
-    ASSERT_EQ(monster.getHealthPoints(),30);
-    ASSERT_EQ(monster.getDamage(),10);
-    ASSERT_EQ(monster.getAttackCoolDown(),0.02);
-
 }
 TEST(all_unitsTest,no_throw_check)
 {
@@ -77,7 +63,7 @@ TEST(all_unitsTest,messed_up_keys)
     Monster unit_messedup{Monster::parse("messedup_keys.json")};
     ASSERT_TRUE(unit_messedup.getName() == "Blood Raven");
     ASSERT_EQ(unit_messedup.getHealthPoints(),113);
-    ASSERT_EQ(unit_messedup.getDamage(),8);
+    ASSERT_EQ(unit_messedup.getphysDamage(),8);
     ASSERT_DOUBLE_EQ(unit_messedup.getAttackCoolDown(),1.2);
 }
 TEST(all_unitsTest,no_throw_fromUnitparser)
