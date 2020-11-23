0fff58f1fabc648f2bfa5989866ff5380a70fc8a
matyko <mesicsmatyas@gmail.com>
2020-11-23 19:46:59 +0100

    Change some on in/out script

--- test/generate_outputs.sh
+++ test/generate_outputs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo `./runMain scenario1.json` >> test/outputs.txt
-echo `./runMain scenario2.json` >> test/outputs.txt
+echo `./a.out scenario1.json` >> test/outputs.txt
+echo `./a.out scenario2.json` >> test/outputs.txt
 
 diff test/outputs.txt test/good_outputs.txt
