ff874a9c29dee766c324f9af5dc24aa371ae8030
matyko <mesicsmatyas@gmail.com>
2020-11-26 13:54:20 +0100

    Fix more conficts

--- Hero.h
+++ Hero.h
@@ -1,28 +1,58 @@
 #pragma once
 class Monster;
 #include "Monster.h"
+#include "Damage.h"
 #include <string>
 
 class Hero
 {
     private:
+        
         int hp; ///< the hp of the Hero
-        int dmg; ///< the damage of the Hero
         const std::string name; ///<The name of the Hero
         double atkcooldown; ///<The atkcooldown of the Hero, which is a number, represents how many second between to attacks.
-        int maxHP; ///< the max HP of the Hero
-        unsigned int xp;///< the Experience of the Hero
-	    unsigned int level;///< the level of the Hero
         unsigned int experience_per_level; ///< How many experience need the Hero to level up.
         unsigned int health_point_bonus_per_level;///< How many bonus health he has per levelup.
         unsigned int damage_bonus_per_level;///< How many bonus damage he has per levelup.
         double cooldown_multiplier_per_level;///< A Number which you multiply with cooldown.
+        double defense;
+        double defbonus_per_level;
+        Damage damage;
+        unsigned int magical_damage_bonus_per_level;
+        int maxHP; ///< the max HP of the Hero
+        unsigned int xp;///< the Experience of the Hero
+	    unsigned int level;///< the level of the Hero
+        
     public:
-        Hero(const int& h, const int& d, const std::string& n, const double& a,
-        const unsigned int& experience_per_level, const unsigned int& health_point_bonus_per_level,
-        const unsigned int& damage_bonus_per_level, const double& cooldown_multiplier_per_level)
-        : hp(h),dmg(d),name(n),atkcooldown(a),maxHP(h), xp(0), level(1), experience_per_level(experience_per_level), health_point_bonus_per_level(health_point_bonus_per_level),
-        damage_bonus_per_level(damage_bonus_per_level), cooldown_multiplier_per_level(cooldown_multiplier_per_level){}
+        Hero(
+        const int& h, 
+        const std::string& n, 
+        double a,
+        const unsigned int& experience_per_level,
+        const unsigned int& health_point_bonus_per_level,
+        const unsigned int& damage_bonus_per_level, 
+        const double& cooldown_multiplier_per_level,
+        const int& def ,
+        const unsigned int& defbonus,
+        Damage dmg,
+        const unsigned int& magic_damage_bonus
+        )
+        : hp(h),
+        name(n),
+        atkcooldown(a),
+        experience_per_level(experience_per_level), 
+        health_point_bonus_per_level(health_point_bonus_per_level),
+        damage_bonus_per_level(damage_bonus_per_level),
+        cooldown_multiplier_per_level(cooldown_multiplier_per_level),
+        defense(def),
+        defbonus_per_level(defbonus),
+        damage(dmg) ,
+        magical_damage_bonus_per_level(magic_damage_bonus)
+        {
+        maxHP=h;
+        xp=0; 
+        level=1;
+        }
         virtual ~Hero(){};
         //!Method that parse Hero Json files and return the generated Hero Object with the parsed data
         /*!
@@ -38,7 +68,7 @@ class Hero
         */
         int getLevel() const;
         //! Method that represents the takeDamage logic from an enemy.
-        void takeDamage(Monster& enemy);
+        void takeDamage(const Monster& enemy);
          //! Simple Getter for hp
         /*!
         \return  integer hp value
@@ -48,7 +78,8 @@ class Hero
         /*!
         \return  const integer damage value
         */
-        int getDamage() const;
+        int getphysDamage() const;
+        int getmagicDamage() const;
          //! Simple Getter for XP
         /*!
         \return const  int XP  value
@@ -77,6 +108,10 @@ class Hero
         */
         bool isAlive() const;
         void boostxp(const int dmg);
+        int getDefense() const;
+        Damage getDamage() const { return damage; }
+        
+        
 
 
 };
--- test/all_unittests.cpp
+++ test/all_unittests.cpp
@@ -14,16 +14,21 @@ TEST(all_unitsTest,hero_load_test)
     Hero hero{Hero::parse("../Dark_Wanderer.json")};
     ASSERT_TRUE(hero.getHealthPoints() == 30);
     ASSERT_TRUE(hero.getName() == "Prince Aidan of Khanduras");
-    ASSERT_TRUE(hero.getDamage() == 3);
+    ASSERT_TRUE(hero.getphysDamage() == 3);
     ASSERT_TRUE(hero.getAttackCoolDown() == 1.1);
+    ASSERT_TRUE(hero.getDefense()== 2);
+    ASSERT_TRUE(hero.getmagicDamage()== 2);
+    ASSERT_TRUE(hero.getXP()== 0); 
 }
 TEST(all_unitsTest,Monster_load_test)
 {
     Monster monster{Monster::parse("../Zombie.json")};
     ASSERT_TRUE(monster.getHealthPoints() == 10);
     ASSERT_TRUE(monster.getName() == "Zombie");
-    ASSERT_TRUE(monster.getDamage() == 1);
+    ASSERT_TRUE(monster.getphysDamage() ==2);
     ASSERT_TRUE(monster.getAttackCoolDown() == 2.8);
+    ASSERT_TRUE(monster.getDefense()== 2);
+    ASSERT_TRUE(monster.getmagicDamage()== 2);
 }
 TEST(all_unitsTest,Unit_edge_case_file)
 {
@@ -39,30 +44,8 @@ TEST(all_unitsTest,typetest)
     EXPECT_EQ(typeid(int),typeid(hero.getHealthPoints()));
     EXPECT_EQ(typeid(std::string),typeid(hero.getName()));
     EXPECT_EQ(typeid(double),typeid(hero.getAttackCoolDown()));
-    EXPECT_EQ(typeid(int),typeid(hero.getDamage()));
+    EXPECT_EQ(typeid(int),typeid(hero.getphysDamage()));
     
-}
-TEST(all_unitsTest,Unit_fight)
-{
-    Hero hero{Hero::parse("../Dark_Wanderer.json")};
-    Monster monster{Monster::parse("../Zombie.json")};
-    hero.fightTilDeath(monster);
-    ASSERT_TRUE(monster.getHealthPoints() < hero.getHealthPoints());
-}
-TEST(all_unitsTest,Generate_hero)
-{
-    Hero hero(30,10,"Hero_of_the_heroes",1.202,2,5,1,0.011);
-    Monster monster(30,10,"Enemy",0.02);
-    ASSERT_EQ(hero.getName(),"Hero_of_the_heroes");
-    ASSERT_EQ(hero.getHealthPoints(),30);
-    ASSERT_EQ(hero.getDamage(),10);
-    ASSERT_EQ(hero.getAttackCoolDown(),1.202);
-
-    ASSERT_EQ(monster.getName(),"Enemy");
-    ASSERT_EQ(monster.getHealthPoints(),30);
-    ASSERT_EQ(monster.getDamage(),10);
-    ASSERT_EQ(monster.getAttackCoolDown(),0.02);
-
 }
 TEST(all_unitsTest,no_throw_check)
 {
@@ -79,7 +62,6 @@ TEST(all_unitsTest,messed_up_keys)
     Monster unit_messedup{Monster::parse("messedup_keys.json")};
     ASSERT_TRUE(unit_messedup.getName() == "Blood Raven");
     ASSERT_EQ(unit_messedup.getHealthPoints(),113);
-    ASSERT_EQ(unit_messedup.getDamage(),8);
     ASSERT_DOUBLE_EQ(unit_messedup.getAttackCoolDown(),1.2);
 }
 TEST(all_unitsTest,no_throw_fromUnitparser)
