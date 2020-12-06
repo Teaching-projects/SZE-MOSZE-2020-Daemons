90cf197efcdb467b72750706e0e8f1115bb95653
kulcsarbence <bance98@gmail.com>
2020-12-06 10:19:08 +0100

    Some fixes

--- Main.cpp
+++ Main.cpp
@@ -22,7 +22,13 @@ const std::map<int,std::string> error_messages = {
     { 1 , "Bad number of arguments. Only a single scenario file should be provided." },
     { 2 , "The provided scenario file is not accessible." },
     { 3 , "The provided scenario file is invalid." },
-    { 4 , "JSON parsing error." }
+    { 4 , "JSON parsing error." },
+    { 5 , "Field occupied exception occured."},
+    { 6 , "Already has hero exception occured."},
+    { 7 , "Already has units exception occured."},
+    { 8 , "Game already started exception occured."},
+    { 9 , "Invalid direction exception occured."},
+    { 10 , "Map already set exception occured."},
 };
 
 void bad_exit(int exitcode){
@@ -41,13 +47,19 @@ int main(int argc, char** argv){
         std::ofstream stream = std::ofstream("log.txt");
         std::string svgrend = "pretty.svg";
         std::string svgrend2 = "pretty_observer.svg";
-        game.registerRenderer(new HeroTextRenderer());  
+        game.registerRenderer(new HeroTextRenderer());
         game.registerRenderer(new ObserverTextRenderer(stream));
         game.registerRenderer(new CharacterSVGRenderer(svgrend));
         game.registerRenderer(new ObserverSVGRenderer(svgrend2));
         game.run();
 
     } catch (const JSON::ParseException& e) {bad_exit(4);}
-    
+    catch(const Game::OccupiedException& e) {bad_exit(5);}
+    catch(const Game::AlreadyHasHeroException& e) {bad_exit(6);}
+    catch(const Game::AlreadyHasUnitsException& e) {bad_exit(7);}
+    catch(const Game::GameAlreadyStartedException& e) {bad_exit(8);}
+    catch(const Game::InvalidDirection& e) {bad_exit(9);}
+    catch(const Game::MapAlreadySet& e) {bad_exit(10);}
+
     return 0;
 }
